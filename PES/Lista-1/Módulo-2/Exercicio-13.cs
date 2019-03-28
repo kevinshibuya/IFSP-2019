1a93590ce12953baf197153227cec35beac633a8
sadkevin <47981817+sadkevin@users.noreply.github.com>
2019-03-28 17:39:22 -0300

    Create Exercicio-13.cs

---
+++ PES/Lista-1/Módulo-2/Exercicio-13.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_20
+{
+    class exercise_20
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, mf;
+
+            Console.Write("Entre a primeira nota: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            mf = (n1 + (n2 * 2)) / 3;
+            
+
+        }
+    }
+}
