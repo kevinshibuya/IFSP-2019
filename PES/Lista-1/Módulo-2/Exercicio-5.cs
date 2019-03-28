da6b313db35ff64dfcaa44f3318dfa07432a487a
sadkevin <47981817+sadkevin@users.noreply.github.com>
2019-03-28 17:37:11 -0300

    Create Exercicio-5.cs

---
+++ PES/Lista-1/Módulo-2/Exercicio-5.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_12
+{
+    class exercise_12
+    {
+        static void Main(string[] args)
+        {
+            int b, h, r;
+
+            Console.Write("Enter the base: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            System.Threading.Thread.Sleep(1000);
+            Console.Write("Enter the high: ");
+            h = Convert.ToInt32(Console.ReadLine());
+            System.Threading.Thread.Sleep(1000);
+
+            r = b * h;
+
+            Console.WriteLine("The area of the rectangle is {0} cm.", r);
+            System.Threading.Thread.Sleep(1000);
+
+            if (r > 100)
+            {
+                Console.WriteLine("That is a big area.");
+            }
+            else
+            {
+                Console.WriteLine("That is a small area.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
