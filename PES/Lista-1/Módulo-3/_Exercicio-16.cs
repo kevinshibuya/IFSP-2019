ca39614f800c6b76f51dd749a796a6d82569aad1
Kevin Shibuya <47981817+sadkevin@users.noreply.github.com>
2019-04-25 17:46:40 -0300

    Create _Exercicio-16.cs

---
+++ PES/Lista-1/Módulo-3/_Exercicio-16.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_38
+{
+    class exercicio_38
+    {
+        static void Main(string[] args)
+        {
+            double n, number, count, lower, higher, average, add, positivePercentage, negativePercentage;
+            string answer;
+            add = 0;
+            higher = 0;
+            average = 0;
+            lower = 10000000000000000000;
+            positivePercentage = 0;
+            negativePercentage = 0;
+            number = 0;
+            count = 1;
+            answer = "Y";
+
+            while (answer == "Y")
+            {
+                count = 1;
+                Console.Write("Enter how many times the loop will repeat: ");
+                number = Convert.ToDouble(Console.ReadLine());
+
+                while (number <= 0)
+                {
+                    Console.WriteLine("Number must be greater than 0.");
+                    Console.Write("Enter number again: ");
+                    number = Convert.ToDouble(Console.ReadLine());
+                }
+
+                while (count <= number)
+                {
+                    Console.Write("Enter the number {0}: ", count);
+                    n = Convert.ToDouble(Console.ReadLine());
+
+                    if (n > higher)
+                    {
+                        higher = n;
+                    }
+                    if (n < lower)
+                    {
+                        lower = n;
+                    }
+
+                    add = add + n;
+
+                    average = add / number;
+
+                    if (n >= 0)
+                    {
+                        positivePercentage += 1;
+                    }
+                    else
+                    {
+                        negativePercentage += 1;
+                    }
+
+                    count++;
+                }
+                positivePercentage = (positivePercentage / number) * 100;
+                negativePercentage = (negativePercentage / number) * 100;
+
+                Console.WriteLine("The highest number is {0}.", higher);
+                Console.WriteLine("The lowest number is {0}.", lower);
+                Console.WriteLine("The addition of all numbers is {0}.", add);
+                Console.WriteLine("The average of all {0} numbers is {1}.", number, average);
+                Console.WriteLine("The positive numbers percentage is {0}%.", positivePercentage);
+                Console.WriteLine("The negative numbers percentage is {0}%.", negativePercentage);
+
+                Console.WriteLine("Enter 'Y' or 'N' ");
+                Console.Write("Do you want to execute the program again? ");
+                answer = Console.ReadLine();
+
+                while (answer != "Y" && answer != "N")
+                {
+                    Console.WriteLine("Enter 'Y' or 'N' ");
+                    Console.Write("Do you want to execute the program again? ");
+                    answer = Console.ReadLine();
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
