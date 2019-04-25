f36c116290695dca93bed344b9790c83b1f23fb1
Kevin Shibuya <47981817+sadkevin@users.noreply.github.com>
2019-04-25 17:46:59 -0300

    Create _Exercicio-17.cs

---
+++ PES/Lista-1/Módulo-3/_Exercicio-17.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_39
+{
+    class exercicio_39
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
