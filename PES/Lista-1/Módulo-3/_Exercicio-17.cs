099f4283697fe3c6657d4087288805c2122cb33d
Kevin Shibuya <47981817+sadkevin@users.noreply.github.com>
2019-05-02 21:14:09 -0300

    Update _Exercicio-17.cs

--- PES/Lista-1/Módulo-3/_Exercicio-17.cs
+++ PES/Lista-1/Módulo-3/_Exercicio-17.cs
@@ -4,12 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace exercicio_39
+namespace modulo_3
 {
-    class exercicio_39
+    class exercicio_17
     {
         static void Main(string[] args)
         {
+            int num1, res, num2;
+            string rep;
+            bool loop;
+            res = 0;
+            loop = true;
+            rep = "Y";
+
+            while(loop)
+            {
+                do {
+                    Console.Write("Enter the factorial number: ");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+                } while (num1 <= 0);
+
+                num2 = num1;
+
+                while(loop) {
+                    res = num1;
+                    num2 = num2 - 1;
+                    num1 = num1 * num2;
+                    if(num2 == 0)
+                    {
+                        loop = false;
+                    }
+                }
+
+                Console.WriteLine("The factorial of the number is {0}", res);
+
+                do {
+                    Console.WriteLine("Want to repeat the proccess?");
+                    Console.WriteLine("Y - yes\nN - no");
+                    rep = Console.ReadLine();
+                } while (rep != "Y" && rep != "N");
+                
+                if(rep == "Y") {
+                    loop = true;
+                }
+            }
+            Console.WriteLine("Proccess ended.");
+            Console.ReadLine();
         }
     }
 }
